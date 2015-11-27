24c38c4c3181f3ab379da1a87c07f87325ca61a4
tangjianye <tangjianye000000@126.com>
2015-11-27 13:45:57 +0800

    增加编译key文件

--- MaskTime/app/src/main/java/com/peach/masktime/utils/CommUtils.java
+++ MaskTime/app/src/main/java/com/peach/masktime/utils/CommUtils.java
@@ -16,29 +16,29 @@ import com.peach.masktime.module.net.API;
 public class CommUtils {
     private static final String TAG = CommUtils.class.getSimpleName();
 
-    public static void setImageUrl(Context cxt, NetworkImageView view, String url) {
+    public static void setImageUrl(Context context, NetworkImageView view, String url) {
         view.setDefaultImageResId(R.drawable.place_holder);
         view.setErrorImageResId(R.drawable.place_holder);
-        view.setImageUrl(API.getPicUrl(url), VolleyManager.getInstance(cxt).getImageLoader());
+        view.setImageUrl(API.getPicUrl(url), VolleyManager.getInstance(context).getImageLoader());
     }
 
     /**
      * 获取应用渠道ID
      *
-     * @param ctx 上下文
+     * @param context 上下文
      * @return
      */
-    public static String getChannel(Context ctx) {
+    public static String getChannel(Context context) {
         ApplicationInfo appInfo = null;
         String channel = null;
         try {
-            appInfo = ctx.getPackageManager().getApplicationInfo(ctx.getPackageName(),
+            appInfo = context.getPackageManager().getApplicationInfo(context.getPackageName(),
                     PackageManager.GET_META_DATA);
         } catch (PackageManager.NameNotFoundException e) {
             e.printStackTrace();
         }
         channel = appInfo.metaData.getString("CHANNEL");
-        Log.d(TAG, " channel == " + channel);
+        Log.i(TAG, " channel == " + channel);
         return channel;
     }
 }
--- MaskTime/app/src/main/java/com/peach/masktime/utils/NetworkUtils.java
+++ MaskTime/app/src/main/java/com/peach/masktime/utils/NetworkUtils.java
@@ -6,15 +6,12 @@ import android.net.NetworkInfo;
 
 public class NetworkUtils {
 
-    public static boolean isNetWorkOk(Context context, boolean toSetting) {
-
-        ConnectivityManager cm = (ConnectivityManager) context
-                .getSystemService(Context.CONNECTIVITY_SERVICE);
+    public static boolean isNetWorkValid(Context context) {
+        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo networkInfo = cm.getActiveNetworkInfo();
         if (networkInfo != null && networkInfo.isConnected()) {
             return true;
         }
         return false;
     }
-
 }
