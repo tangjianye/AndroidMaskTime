8cc53822589b435d70fd31719c25e2ba1a5a1574
tangjianye <tangjianye000000@126.com>
2015-12-30 09:40:15 +0800

    增加数据库
    增加数据库

---
+++ MaskTime/AndroidDaoGenerator/src/main/java/de/greenrobot/daogenerator/gentest/AndroidDaoGenerator.java
@@ -0,0 +1,22 @@
+package de.greenrobot.daogenerator.gentest;
+
+import de.greenrobot.daogenerator.DaoGenerator;
+import de.greenrobot.daogenerator.Entity;
+import de.greenrobot.daogenerator.Schema;
+
+public class AndroidDaoGenerator {
+    public static void main(String[] args) throws Exception {
+        Schema schema = new Schema(1, "com.peach.masktime.db");
+
+        addNote(schema);
+        new DaoGenerator().generateAll(schema, "app/src/main/java");
+    }
+
+    private static void addNote(Schema schema) {
+        Entity note = schema.addEntity("Note");
+        note.addIdProperty();
+        note.addStringProperty("text").notNull();
+        note.addStringProperty("comment");
+        note.addDateProperty("date");
+    }
+}
